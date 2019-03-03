d97c7de3152159ac637654119b92d145938120eb
buttch33k <nlacsamana09@gmail.com>
2019-03-03 14:28:41 +0800

    initialized project

---
+++ Draw.java
@@ -0,0 +1,122 @@
+import javax.swing.JComponent;
+import javax.swing.Timer;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+
+public class Draw extends JComponent{
+
+	private BufferedImage image;
+	public URL resource = getClass().getResource("f0.png");
+
+	// circle's position
+	public int x = 30;
+	public int y = 30;
+
+	// animation states
+	public int state = 0;
+
+	public Draw(){
+		try{
+			image = ImageIO.read(resource);
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void reloadImage(){
+		state++;
+
+		if(state == 0){
+			resource = getClass().getResource("f0.png");
+		}
+		else if(state == 1){
+			resource = getClass().getResource("f1.png");
+		}
+		else if(state == 2){
+			resource = getClass().getResource("f2.png");
+		}
+		else if(state == 3){
+			resource = getClass().getResource("f3.png");
+		}	
+		else if(state == 4){
+			resource = getClass().getResource("f4.png");
+		}
+		else if(state == 5){
+			resource = getClass().getResource("f5.png");
+		}	
+		try{
+			image = ImageIO.read(resource);
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	public void attackAnimation(){
+		Thread thread1 = new Thread(new Runnable(){
+			public void run(){
+				for(int ctr = 0; ctr < 4; ctr++){
+					try {
+						if(ctr==3){
+							resource = getClass().getResource("f0.png");
+						}
+						else{
+							resource = getClass().getResource("fattack"+ctr+".png");
+						}
+						
+						try{
+							image = ImageIO.read(resource);
+						}
+						catch(IOException e){
+							e.printStackTrace();
+						}
+				        repaint();
+				        Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		thread1.start();
+	}
+
+	public void attack(){
+		attackAnimation();
+	}
+
+	public void moveUp(){
+		y = y - 5;
+		reloadImage();
+		repaint();
+	}
+
+	public void moveDown(){
+		y = y + 5;
+		reloadImage();
+		repaint();
+	}
+
+	public void moveLeft(){
+		x = x - 5;
+		reloadImage();
+		repaint();
+	}
+
+	public void moveRight(){
+		x = x + 5;
+		reloadImage();
+		repaint();
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.setColor(Color.YELLOW);
+		g.drawImage(image, x, y, this);
+	}
+}
